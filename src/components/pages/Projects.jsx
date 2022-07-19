491ffa29057d9739ffd27272233f080d5f0dec4f
s2200120 <s2200120@edu.bc.fi>
2022-07-19 03:02:58 +0300

    search created in projects page

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import projects from './dummydb/dummy_projects';
+import projects from './dummydb/dummy_projects'; //replace this with axios
 import Project from '../UIs/Project';
-//import { useState } from 'react';
+import { useState } from 'react';
 
 function Projects() {
-    //const [stateProjects, setStateProjects] = useState({});
+    // const [projects, setProjects] = useState({});
+    const [search, setSearch] = useState('');
+    const projectsFilter = projects.filter((res) => {
+        return res.title.toLowerCase().includes(search.toLowerCase());
+      });
     
+      const searchHandler = (e) => {
+        setSearch(e.target.value); 
+        };
+
   return (
     <div className='page'>
        <div className="content">
         <h1>"Projects"</h1>
+        <div className="search">
+            <input type="text" placeholder='Search...' onChange={searchHandler}/>
+        </div>
+        
         <div className="projects"
             style={{
                 width:"80vw",
                 minHeight:"100vh",
                 display:"flex",
                 flexWrap: "wrap",
-                
             }}
         >
-            {projects.map(project => (
+            
+
+            {projectsFilter.map(project => (
                 <Project 
                 key={project.id} 
                 id={project.id} 
