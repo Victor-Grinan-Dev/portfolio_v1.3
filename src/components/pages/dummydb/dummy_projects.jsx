3b551fbf151058bb34d6253a5a1b7ab76641deae
s2200120 <s2200120@edu.bc.fi>
2022-07-19 04:41:19 +0300

    created the image spot for projects

--- src/components/pages/dummydb/dummy_projects.jsx
+++ src/components/pages/dummydb/dummy_projects.jsx
@@ -5,14 +5,15 @@ import hexMap from '../../assets/images/project_images/hexMap.JPG';
 import hairPic from '../../assets/images/project_images/mamis_hairdo.JPG';
 import speedPic from '../../assets/images/project_images/speedgame.JPG';
 import bloodBowlPic from '../../assets/images/project_images/turn_timer.JPG';
+import galleryPic from '../../assets/images/project_images/gallery.png'
 
 const projects = [
     {
         id:1,
         title:"BMI_calc",
         description: "an app to control diet",
-        image:"../../assets/images/project_images/bmi_calc.png",
-        url: bimPic,
+        image: bimPic,
+        url:"https://victor-grinan-dev.github.io/BMI_calc/",
         main_lang:"Javascript",
         type:"school"
     },
@@ -20,8 +21,8 @@ const projects = [
         id:2,
         title:"Gradients",
         description: "a gradients background color controller",
-        image:"../../assets/images/project_images/02gradients.JPG",
-        url: gradientsPic,
+        image: gradientsPic,
+        url:"https://victor-grinan-dev.github.io/color_gradients/",
         main_lang:"Javascript",
         client:"school"
     },
@@ -29,8 +30,8 @@ const projects = [
         id:3,
         title:"Speed Game",
         description: "a little game to lear state",
-        image:"",
-        url: speedPic,
+        image: speedPic,
+        url:"https://victor-grinan-dev.github.io/speed_drum/",
         main_lang:"Javascript",
         client:"school"
     },
@@ -49,7 +50,7 @@ const projects = [
         title:"ToDo List",
         description: "keep track of your ToDo's",
         image:"",
-        url: "",
+        url:"https://victor-grinan-dev.github.io/ToDoList/",
         main_lang:"Javascript",
         client:"school"
     },
@@ -73,7 +74,7 @@ const projects = [
         id:8,
         title:"Gallery",
         description: "a images gallery",
-        image:"",
+        image:galleryPic,
         url:"https://victor-grinan-dev.github.io/gallery/",
         main_lang:"Javascript"
     },
@@ -81,21 +82,21 @@ const projects = [
         id:9,
         title:"blood bowl timer",
         description: "blood boooowl!!!",
-        image:"",
+        image: bloodBowlPic,
         url:"https://victor-grinan-dev.github.io/blood_bowl_turn_timer/",
         main_lang:"Javascript"
     },
     {
         id:10,
-        title:"personal project 1",
+        title:"hexMap",
         description: "personal app",
-        image:"",
+        image: hexMap,
         url:"",
         main_lang:"Javascript"
     },
     {
         id:11,
-        title:"currencies",
+        title:"keeper app",
         description: "personal app",
         image:"",
         url:"",
@@ -103,11 +104,11 @@ const projects = [
     },
     {
         id:12,
-        title:"personal project 2",
+        title:"mami's hair do",
         description: "personal app",
-        image:"",
+        image: hairPic,
         url:"",
-        main_lang:"python"
+        main_lang:"javascript"
     },
     {
         id:13,
